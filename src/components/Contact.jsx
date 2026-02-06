5e2156e4fe2d25b3448d45e54e397a3a13b51ceb
jihadcodes <jihad.dev.pro@gmail.com>
2026-02-07 01:42:28 +0600

    input field updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
                     <p className='font-montserrat font-normal text-[20px] text-[#666666] pb-7.5 text-center'>Sign up to receive inspiration, product updates, and special offers from our team.</p>
 
                     <div className='flex mx-auto'>
-                        <input type="text" placeholder='example@gmail.com' className='border border-[#054C73] py-6.75 pr-33.25 pl-7 rounded-tl-[3px] rounded-bl-[3px] text-[#011C42] font-montserrat font-normal text-[16px] bg-[#FFFFFF]' />
+                        <input type="text" placeholder='example@gmail.com' className='border outline-0 border-[#054C73] py-6.75 pr-33.25 pl-7 rounded-tl-[3px] rounded-bl-[3px] text-[#011C42] font-montserrat font-normal text-[16px] bg-[#FFFFFF]' />
                         <button className='border border-[#054C73] py-6.25 px-11.25 rounded-tr-[3px] rounded-br-[3px] text-[#FFFFFF] bg-[#054C73]'>Submit</button>
                     </div>
                 </div>
